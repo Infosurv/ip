8b1d2e4ba57eaa92c2493212e546e537fe63ee2c
peb7268 <peb7268@gmail.com>
2015-09-21 08:09:08 -0400

    newrelic wip

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['IntengoPear'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'db8297d0c490eb94c7d5407cbeac52b5e53e92d7',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
